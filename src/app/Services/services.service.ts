293c05ddc83c1f904ba931a5579be08a063e7bf4
Lê Văn Võ <vole543215@gmail.com>
2023-08-04 09:08:34 +0700

    fix-data-to deploy on internet.

--- src/app/Services/services.service.ts
+++ src/app/Services/services.service.ts
@@ -9,39 +9,45 @@ import { ILogin } from '../typeUser';
 })
 export class ServiceService {
   constructor(private http:HttpClient) { }
-
+  URL_users=`https://n4l3tr-3000.csb.app/users`;
+  URL_message=`https://n4l3tr-3000.csb.app/message`;
 
   // ==============================User
   getAllUser():Observable<ILogin[]>{// getting All Users
-    return this.http.get<ILogin[]>(`http://localhost:3000/users`);
+    return this.http.get<ILogin[]>(`${this.URL_users}`);
   }
   getOneUser(idUser:any):Observable<ILogin[]>{// getting All Users
-    return this.http.get<ILogin[]>(`http://localhost:3000/users/${idUser}`);
+    return this.http.get<ILogin[]>(`${this.URL_users}/${idUser}`);
   }
   addUser(dataUser:any):Observable<ILogin[]>{// getting All Users
-    return this.http.post<ILogin[]>(`http://localhost:3000/users`,dataUser);
+    return this.http.post<ILogin[]>(`${this.URL_users}`,dataUser);
   }
   updateUser(dataUser:any):Observable<ILogin[]>{// getting All Users
-    return this.http.put<ILogin[]>(`http://localhost:3000/users/${dataUser.id}`,dataUser);
+    return this.http.put<ILogin[]>(`${this.URL_users}/${dataUser.id}`,dataUser);
   }
   
   // ==============================Message
   getAllMessage():Observable<any[]>{// getting All Messages
-    return this.http.get<any[]>(`http://localhost:3000/message`);
+    return this.http.get<any[]>(`${this.URL_message}`);
   }
   getOneMessage(idMessage:any):Observable<any[]>{// getting one Messages
-    return this.http.get<any[]>(`http://localhost:3000/message/${idMessage}`);
+    return this.http.get<any[]>(`${this.URL_message}/${idMessage}`);
   }
   addMessage(dataMessage:any):Observable<any[]>{// add Messages
-    return this.http.post<any[]>(`http://localhost:3000/message`,dataMessage);
+    return this.http.post<any[]>(`${this.URL_message}`,dataMessage);
   }
   updateMessage(dataMessage:any):Observable<any[]>{// update Messages
-    return this.http.put<any[]>(`http://localhost:3000/message/${dataMessage.id}`,dataMessage);
+    return this.http.put<any[]>(`${this.URL_message}/${dataMessage.id}`,dataMessage);
   }
   deleteMessage(idMessage:any):Observable<any[]>{// delete one Messages
-    return this.http.delete<any[]>(`http://localhost:3000/message/${idMessage}`);
+    return this.http.delete<any[]>(`${this.URL_message}/${idMessage}`);
   }
 
+
+  // =========================================================================================================
+  test(){
+    return this.http.get("https://localhost:5001/api/users");
+  }
 
 
 
